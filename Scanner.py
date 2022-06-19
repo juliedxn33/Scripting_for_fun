9110698087e1e4a95a8cfd917f00aaad391e3df6
juliedxn <juliedxn33@gmail.com>
2022-06-19 14:34:19 -0500

    update

--- Scanner.py
+++ Scanner.py
@@ -4,6 +4,8 @@ import subprocess
 import os
 import sys
 from pathlib import Path
+import xml.etree.ElementTree as Xet
+import pandas as pdpyt
 
 def Hosts_Up_Check():
 	check_hosts_input_file = sys.argv[1]
@@ -18,7 +20,6 @@ def Hosts_Up_Check():
 		while True:
 			with open('hosts_up.txt', 'a') as f: 	 
 				output = process.stdout.readline()
-				print(output)
 				hosts.append(output)
 				if 'Host is up' in output:
 					ip = (hosts[-2])
@@ -32,24 +33,17 @@ def Hosts_Up_Check():
 
 	
 def Nmap_Scan(f):
-	hosts_up_file = open('hosts_up.txt', 'r')
+	hosts_up_file = open(f, 'r')
+	print(f)
 	while True: 
 		hosts_up_ip = hosts_up_file.readline()
 		if not hosts_up_ip:
 			break
 		hosts_up_ip = hosts_up_ip.strip()
-		print(hosts_up_ip)
+		process = subprocess.run(['nmap', '-A', '-sV', '-p-', hosts_up_ip, '-oX', 'nmap_output.xml'], stdout=subprocess.PIPE, text=True)
+		break
+	return process.stdout
 	
-		with open('nmap_scan.txt', 'a') as f:
-			print(f"hosts up: {hosts_up_ip}")
-			f.write(hosts_up_ip)
-			process = subprocess.run(['nmap', '-A', '-sV', '-p-', hosts_up_ip], stdout = f)
-	f.close()
-	return f
-
-
-def Nmap_Clean_Up(f):
-
 
 def main():
 	hosts_up = Path('hosts_up.txt')
