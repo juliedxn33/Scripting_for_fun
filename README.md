cbb0f2cc44634ca9e04f20d982dadd3ffe44a63b
juliedxn33 <juliedxn33@gmail.com>
2022-06-26 21:09:12 -0500

    functional xmltocsv module for one ip

---
+++ README.md
@@ -0,0 +1,26 @@
+Thanks for taking the time to look at my project!
+
+I am currently working a scanner to help automate the enumeration phase of a pen-test.
+NOTE: autorecon and python-nmap already have professional code to accomplish some of the functionality I am working on. 
+I am just doing this for fun and for practice.
+
+Requirements: 
+	python 3.10.x
+	nmap
+	xmltodict
+	pandas
+	
+
+Below are the functions I have created so far and upcoming goals
+
+Current functions:
+- ping sweep to check for hosts that are up
+- nmap launch on resulting hosts for all ports with verbose output, saving results to xml file 
+- xml to csv for more readable output
+
+Goals:
+- dirb for all 443 and 80 ports
+- xxs, sql injection, xsrf modules
+- launching other nmap scripts on susceptible services
+- multi-threaded processing for quicker turnaround time 
+	-(threads will be implemented last but is a must! Scans take an incredible amount of time and pen-tests are typically time-crunched as is)
--- Scanner.py
+++ Scanner.py
@@ -5,9 +5,11 @@ import os
 import sys
 from pathlib import Path
 import xmltodict
+import pandas as pd
 
-def Hosts_Up_Check():
-	check_hosts_input_file = sys.argv[1]
+#uses nmap -sP to check if hosts are live and saves corresponding hosts to hosts_up.txt
+def Hosts_Up_Check(hosts_input_file):
+	check_hosts_input_file = hosts_input_file
 	check_hosts_file = open(check_hosts_input_file, 'r')
 	while True: 
 		check_hosts_ip = check_hosts_file.readline()
@@ -30,30 +32,64 @@ def Hosts_Up_Check():
 	return f	
 	
 
-	
+#launches a verbose nmap scan on the hosts in the "hosts_up.txt", scans all ports (tcp only) and outputs results into an xml file
 def Nmap_Scan():
-	process = subprocess.run(['nmap', '-A', '-sV', '-p-', '-iL', 'hosts_up.txt', '-oX', 'nmap_xml_output.xml'], universal_newlines=True)
+	process = subprocess.run(['nmap', '-A', '-sV', '-v', '-p-', '-iL', 'hosts_up.txt', '-oX', 'nmap_xml_output.xml'], universal_newlines=True)
 	xml_output = 'nmap_xml_output.xml'
 	return xml_output
 
-def Nmap_Clean_Up(f_name):
+#cleans up "nmap_xml_output.xml" by using xmltodict library to parse data into a dict. The dict results are stored in a readable csv file. Full functionality is still being worked on.
+def Nmap_Clean_Up():
 	with open('nmap_xml_output.xml', 'r') as xml_obj:
 		nmap_dict = xmltodict.parse(xml_obj.read())
 		xml_obj.close()	
 		
 	with open('xmltocsv_tool_output', 'w') as xml_debug:
 		xml_debug.write(str(nmap_dict))	
-	#print(f"I have reached this location \n\n\n {nmap_dict}")	
-	print(f"host info address: {nmap_dict['nmaprun']['host']['address']['@addr']}")
-	print(f"host info address type: {nmap_dict['nmaprun']['host']['address']['@addrtype']}")
-	print(f"host info hostname: {nmap_dict['nmaprun']['host']['hostnames']['hostname']['@name']}")
 	
-	print(f"host info port: {nmap_dict['nmaprun']['host']['ports']['port'][0]['@protocol']}")
-	print(f"host info port: {nmap_dict['nmaprun']['host']['ports']['port'][0]['@portid']}")
-	print(f"host info protocol: {nmap_dict['nmaprun']['host']['ports']['port'][0]['@protocol']}")
-	print(f"host info state: {nmap_dict['nmaprun']['host']['ports']['port'][0]['state']['@state']}")
+	#TODO development has been done nmapping only localhost due to lack of lab environment, how to accommodate multiple hosts?
+			
+	rows = []
+	cols = ["Host Address", "Address Type", "Hostname", "Port", "Protocol", "State"]
 	
+	#troubleshooting print statements	
+	#print(f"host info address: {nmap_dict['nmaprun']['host']['address']['@addr']}")
+	#print(f"host info address type: {nmap_dict['nmaprun']['host']['address']['@addrtype']}")
+	#print(f"host info hostname: {nmap_dict['nmaprun']['host']['hostnames']['hostname']['@name']}")
 	
+	addr = str({nmap_dict['nmaprun']['host']['address']['@addr']})
+	addr = addr.strip("{}")
+	addrtype = str({nmap_dict['nmaprun']['host']['address']['@addrtype']})
+	addrtype = addrtype.strip("{}")
+	name = str({nmap_dict['nmaprun']['host']['hostnames']['hostname']['@name']})
+	name = name.strip("{}")
+	#TODO cycle through x ports for all port info
+	try:
+		
+		#troubleshooting print statements
+		#print(f"host info port: {nmap_dict['nmaprun']['host']['ports']['port']['@portid']}")
+		#print(f"host info protocol: {nmap_dict['nmaprun']['host']['ports']['port']['@protocol']}")
+		#print(f"host info state: {nmap_dict['nmaprun']['host']['ports']['port']['state']['@state']}")
+		
+		
+		portid = str({nmap_dict['nmaprun']['host']['ports']['port']['@portid']})
+		portid = portid.strip("{}")
+		protocol = str({nmap_dict['nmaprun']['host']['ports']['port']['@protocol']})
+		protocol = protocol.strip("{}")
+		state = str({nmap_dict['nmaprun']['host']['ports']['port']['state']['@state']})
+		state = state.strip("{}")
+		
+		rows.append({"Host Address": addr, "Address Type": addrtype, "Hostname": name, "Port": portid, "Protocol": protocol, "State" : state})
+              
+	
+	except KeyError:
+		print("Sometimes xml does weird dict stuff this might be one of those times")
+
+
+	df = pd.DataFrame(rows, columns=cols)
+	# Writing dataframe to csv
+	df.to_csv('xml_to_csv_output.csv')
+
 
 
 def main():
@@ -63,10 +99,18 @@ def main():
 		os.remove(hosts_up)
 	if nmap_scan.is_file():
 		os.remove(nmap_scan)
-	#Hosts_Up_Check()
-	#Nmap_Scan()
-	Nmap_Clean_Up(Nmap_Scan())
-
+	#fully automated option where input is just a text file with ipv4 hosts
+	full_auto = "-a"	
+	if sys.argv[1] == full_auto:
+		try:
+			check_hosts_input_file = sys.argv[2]	
+			Hosts_Up_Check(check_hosts_input_file)
+			Nmap_Scan()
+			Nmap_Clean_Up()
+		except IndexError:
+			print("Text file with hosts missing")
+			exit()
+			
 
 if __name__== '__main__':
 	main()		
